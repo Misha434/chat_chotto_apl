61711bd5a5dd0bc448b45f4e76c6c48b2a5d12a6
misha434 <missymouse434@gmail.com>
2022-07-17 19:00:06 +0900

    initialize MUI setting

--- src/components/Sample1.tsx
+++ src/components/Sample1.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@mui/material";
 import React from "react";
 
 const Sample1: React.FC = () => {
-  return <h1>Sample</h1>;
+  return (
+    <>
+      <h1>Sample</h1>
+      <Button variant="contained">Hello</Button>
+    </>
+  );
 };
 
 export default Sample1;
