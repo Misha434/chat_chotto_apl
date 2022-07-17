d59e46aada5581a0dda89e666aaeb54b4fc0fe66
misha434 <missymouse434@gmail.com>
2022-07-17 18:13:15 +0900

    trial commit

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home: React.FC = () => {
   return (
     <>
       <div>Home</div>
-      <Link to="/sample1">sample1</Link>
+      <Link to="/sample1">example1</Link>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
